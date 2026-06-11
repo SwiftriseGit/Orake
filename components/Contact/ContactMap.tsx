3f38f2ac0d8666f08621e72081d11444a8d3bbac
sunil8521 <heyitsmevro@gmail.com>
2026-06-11 17:03:56 +0530

    update: update embedded map coordinates on contact page

--- components/Contact/ContactMap.tsx
+++ components/Contact/ContactMap.tsx
@@ -3,7 +3,7 @@ export default function ContactMap() {
     <div className="rounded-3xl overflow-hidden border border-gray-100 shadow-sm w-full h-[400px] sm:h-[500px]">
       <div className="relative h-full w-full bg-gray-100">
         <iframe
-          src="https://maps.google.com/maps?q=28.631451,77.206331&z=15&output=embed"
+          src="https://maps.google.com/maps?q=21.330945,83.6383182&z=17&output=embed"
           width="100%"
           height="100%"
           style={{ border: 0 }}
